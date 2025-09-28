caf2f1e6eeba7564744abf46323fc93aa1f90e45
Marc Manuel <marc_manuel@hotmail.com>
2025-09-27 23:46:02 -0400

    fix issue with render not supporting .net 9

--- Program.cs
+++ Program.cs
@@ -1,15 +1,18 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
 }
 
 app.UseHttpsRedirection();
